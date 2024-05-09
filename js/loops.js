e26b42589002f7cab21e4509307c3667b0fe2088
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-05-09 16:48:07 +0500

    created

---
+++ js/loops.js
@@ -0,0 +1,78 @@
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+// console.log(50);
+
+
+// arithmetic statement has 3 types -> unary , binary , ternary
+
+// symbols/characters/operators are (+ - * / % ! || && > < ? : ^ = == === ) 
+
+// For loop contain 3 parts in parenthesis
+// 1. initialization    2. condition    3. increment
+// for (let i = 0; i < 10; i++) {
+//     console.log(10);
+
+//     console.log(50);
+// }
+
+let i = 5;
+
+i++;  // increment
+    // i = i + 2;
+    i+=2;
+    i = i + 2;
+    i-=2;
+    i*=2;
+    // i/=4;
+    
+    i--;  // decrement
+    // i = i - 1;
+    ++i;
+    --i;
+    let j = 15;
+
+
+
+    
+    // console.log(i);
+
+    let array = [15, 16];
+    let array2 =  array;
+
+    // console.log(array);
+    // console.log(array2);
+    
+    array[0] = 20;
+    
+    // console.log(array);
+    // console.log(array2)
+    
+    array2[0] = 25;
+    
+    // console.log(array)
+    // console.log(array2);
+    
+    array2 = [25, 16];
+    
+    
+    
+    let f = 15;
+    let g = "15";
+
+    console.log(f)
+    console.log(g)
+
+    console.log(f === g);
+
+    
+    // console.log(array);
+    // console.log(array2);
+    // console.log(array === array2)
+// console.log(i === j);
